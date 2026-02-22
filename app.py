75fc6590f759e6695b2d9e7f951911aa7fe301ea
FrancoC94 <cj.francom@uea.edu.ec>
2026-02-22 09:35:23 -0500

    git commit -m "Mi proyecto Flask"

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    return "Bienvenido a Tienda Online – Catálogo y ofertas"
+
+@app.route('/usuario/<nombre>')
+def usuario(nombre):
+    return f'Bienvenido, {nombre}!'
+
+@app.route('/producto/<nombre>')
+def producto(nombre):
+    return f'Producto: {nombre} – disponible en tienda'
+
+if __name__ == '__main__':
+    app.run(debug=True)
