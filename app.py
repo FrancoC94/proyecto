f52f6149084f2c9757867dbc617b4c6a1d18517f
FrancoC94 <cj.francom@uea.edu.ec>
2026-03-01 22:03:46 -0500

    Mi Proyecto Flask

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Ruta principal
 @app.route('/')
 def inicio():
-    return "Bienvenido a Tienda Online – Catálogo y ofertas"
+    return render_template('index.html')
 
+# Ruta acerca de
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Ruta dinámica usuario
 @app.route('/usuario/<nombre>')
 def usuario(nombre):
-    return f'Bienvenido, {nombre}!'
+    return render_template('index.html', nombre=nombre)
 
+# Ruta dinámica producto
 @app.route('/producto/<nombre>')
 def producto(nombre):
     return f'Producto: {nombre} – disponible en tienda'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
