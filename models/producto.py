408c5e2a8b927e6a634fe49fc3e49fc587f678ee
FrancoC94 <cj.francom@uea.edu.ec>
2026-03-29 20:40:25 -0500

    Create producto.py
    Mi proyecto Flask

---
+++ models/producto.py
@@ -0,0 +1,6 @@
+class Producto:
+    def __init__(self, id, nombre, precio, cantidad):
+        self.id = id
+        self.nombre = nombre
+        self.precio = precio
+        self.cantidad = cantidad
